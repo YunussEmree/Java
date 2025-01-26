a8b50264958e7e0f23c42bfdb269ffa7c9ee137f
YunussEmree <senyigityunusemre@gmail.com>
2025-01-26 07:29:13 +0300

    Added spring ai example

---
+++ Spring/SpringAI/src/main/java/com/yunussemree/springai/ChatController.java
@@ -0,0 +1,22 @@
+package com.yunussemree.springai;
+
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatController {
+
+    private final ChatModel chatModel;
+
+    public ChatController(ChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+
+    @GetMapping("/chat")
+    String generation(@RequestParam String userInput) {
+        return chatModel.call(userInput);
+    }
+}
